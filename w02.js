a378831a6c557922a130d8d4cb6b753e2f1ddbdb
EC2 Default User <ec2-user@ip-172-31-45-223.ec2.internal>
2018-09-14 16:05:54 +0000

    first commit

---
+++ w02.js
@@ -0,0 +1,22 @@
+// npm install cheerio
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the thesis text file into a variable, `content`
+var content = fs.readFileSync('data/thesis.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// print names of thesis students
+$('h3').each(function(i, elem) {
+    console.log($(elem).text());
+});
+
+console.log('* * * * * * * * * *');
+
+// print project titles
+$('.project .title').each(function(i, elem) {
+    console.log($(elem).text());
+});
